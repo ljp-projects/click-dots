7cac209d99357f832f72a4f6431c9224eda4819c
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-11-30 07:50:30 +1100

    oh no

---
+++ index.js
@@ -0,0 +1,48 @@
+const sizes = {
+	width: document.documentElement.clientWidth,
+  height: document.documentElement.clientHeight,
+  relativeWidth: p => sizes.width * p,
+  relativeHeight: p => sizes.height * p
+}
+
+console.log(sizes.relativeWidth(1))
+
+const dots = [
+	{ x: sizes.relativeWidth(7/8), y: sizes.relativeHeight(1/5), draw: true },
+  { x: sizes.relativeWidth(7/8), y: sizes.relativeHeight(2/5), draw: true },
+  { x: sizes.relativeWidth(7/8), y: sizes.relativeHeight(3/5), draw: true },
+  { x: sizes.relativeWidth(7/8), y: sizes.relativeHeight(4/5), draw: true }
+]
+
+const drawDots = () => {
+	dots.forEach((dot, index) => {
+  	if (!dot.draw) return
+  	const el = document.createElement('div')
+    el.setAttribute('class', 'dot')
+    el.style.top = `${dot.y}px`
+    el.style.left = `${dot.x}px`
+    
+    document.body.appendChild(el)
+    
+    setTimeout(() => {
+    	el.remove()
+    }, 1/25)
+    
+    el.onclick = () => {
+    	console.log("E")
+      dot.draw = false
+    }
+    
+    if (dot.x >= -10) {
+    	dot.x -= 1/6 * (60 - index * 2)
+    } else {
+      const a = new Audio("https://bigsoundbank.com/UPLOAD/mp3/0280.mp3")
+      a.play()
+    	dot.x = sizes.relativeWidth(7/8)
+    }
+  })
+  
+  setTimeout(drawDots, 1/25)
+}
+
+drawDots()
