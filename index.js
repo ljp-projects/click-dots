0989b9ba4cc3254962b2a0edf1aced24f3a8eee2
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-11-30 17:42:30 +1100

    ooo

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ const drawDots = async () => {
       dot.draw = false
     }
     
-    if (dot.x === 0) {
+    if (dot.x <= 0) {
     	dot.x -= -(1/6 * (60 - index * 2))
-    } else if (dot.x === sizes.width) {
+    } else if (dot.x >= sizes.width) {
       dot.x -= (1/6 * (60 - index * 2))
     } else {
       new Audio("./ping.mp3").play()
